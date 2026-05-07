18c6b4ad5899d1203effce5877726686f388bfc5
Izdz-Ctrl <izdzmikokomi@gmail.com>
2026-05-07 09:43:31 +0700

    feat: setup initial CI/CD workflow

---
+++ src/test/java/bank_system/BankTest.java
@@ -0,0 +1,13 @@
+package bank_system;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BankTest {
+  @Test
+  public void testInitialBalance() {
+    // Kiểm tra xem tài khoản khởi tạo có đúng số dư không
+    Account account = new CheckingAccount(101, 500.0);
+    assertEquals(500.0, account.getBalance(), "Số dư khởi tạo phải là 500");
+  }
+}
